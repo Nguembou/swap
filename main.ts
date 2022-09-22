c24d6a226617c037901cf83fbe4fdb2c472caeaf
Nguembou <113910948+Nguembou@users.noreply.github.com>
2022-09-22 09:24:37 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    N1 += 1
+    basic.showNumber(N1)
+})
+input.onButtonPressed(Button.B, function () {
+    N1 += -1
+    basic.showNumber(N2)
+})
+let N2 = 0
+let N1 = 0
+N1 = 0
+N2 += 0
+basic.forever(function () {
+	
+})
